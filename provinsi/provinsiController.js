569337c625992b3221a68c3326cb2d91cd4d76c3
Rudi Anton <32452075+Rudi-Anton@users.noreply.github.com>
2017-10-20 17:54:46 +0700

    Update provinsiController.js

--- provinsi/provinsiController.js
+++ provinsi/provinsiController.js
@@ -11,6 +11,9 @@ module.exports.removeProvinsi=function(_id,callback){
 module.exports.updateProvinsi=function(_id,provinsi,callback){
     Provinsi.findByIdAndUpdate(_id,provinsi,callback);
 }
+module.exports.getByNamaProvinsi=function(id,callback){
+    Provinsi.find({NamaProvinsi:id},callback);
+}
 module.exports.getProvinsiById=function(id,callback){
     Provinsi.findById(id,callback);
 }
